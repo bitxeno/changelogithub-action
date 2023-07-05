41c3214f82d281f2a37e87a1fe4557580d41ebb4
bitxeno <137328844+bitxeno@users.noreply.github.com>
2023-07-05 21:26:02 +0800

    feat: change log text

--- src/libs/changelog.ts
+++ src/libs/changelog.ts
@@ -22,7 +22,8 @@ export async function generateChangelog() {
       throw new Error(`Current ref "${config.to}" is not available as tags on GitHub. Release skipped.`)
     }
 
-    let changelog = md.replace('##### &nbsp;&nbsp;&nbsp;&nbsp;', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**Full Changelog**: ')
+    let changelog = md.replace('### &nbsp;&nbsp;&nbsp;', '## ')
+                      .replace('##### &nbsp;&nbsp;&nbsp;&nbsp;', '**Full Changelog**: ')
                       .replace('View changes on GitHub', `${config.from}...${config.to}`)
     setChangelogOutput(changelog)
 
